12863d64d5b726a0eec85d6c15501502e8654ddd
Zhuo-Fei Hui <fei@posteo.de>
2018-09-26 00:15:18 +0200

    Sorting

--- app/main.js
+++ app/main.js
@@ -2,5 +2,12 @@ new Vue({
   el: "#app",
   data: {
     submissions: Seed.submissions
+  },
+  computed: {
+    sortedSubmissions () {
+      return this.submissions.sort((a, b) => {
+        return b.votes - a.votes
+      });
+    }
   }
 });
