624c59ed0866668e518bbbd0aecd168181a1972d
Zhuo-Fei Hui <fei@posteo.de>
2018-09-25 23:59:45 +0200

    Initial skeleton

---
+++ app/seed.js
@@ -0,0 +1,42 @@
+window.Seed = (function () {
+  const submissions = [
+    {
+      id: 1,
+      title: 'Yellow Pail',
+      description: 'On-demand sand castle construction expertise.',
+      url: '#',
+      votes: 16,
+      avatar: '../public/images/avatars/daniel.jpg',
+      submissionImage: '../public/images/submissions/image-yellow.png',
+    },
+    {
+      id: 2,
+      title: 'Supermajority: The Fantasy Congress League',
+      description: 'Earn points when your favorite politicians pass legislation.',
+      url: '#',
+      votes: 11,
+      avatar: '../public/images/avatars/kristy.png',
+      submissionImage: '../public/images/submissions/image-rose.png',
+    },
+    {
+      id: 3,
+      title: 'Tinfoild: Tailored tinfoil hats',
+      description: 'We have your measurements and shipping address.',
+      url: '#',
+      votes: 17,
+      avatar: '../public/images/avatars/veronika.jpg',
+      submissionImage: '../public/images/submissions/image-steel.png',
+    },
+    {
+      id: 4,
+      title: 'Haught or Naught',
+      description: 'High-minded or absent-minded? You decide.',
+      url: '#',
+      votes: 9,
+      avatar: '../public/images/avatars/molly.png',
+      submissionImage: '../public/images/submissions/image-aqua.png',
+    }
+  ];
+
+  return { submissions: submissions };
+}());
