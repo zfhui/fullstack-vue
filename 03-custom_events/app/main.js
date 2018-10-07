96134b810c4f2c174c1c67f09b1db9178965a830
Zhuo-Fei Hui <fei@posteo.de>
2018-10-07 21:32:20 +0200

    Add placeholder to input field

--- 03-custom_events/app/main.js
+++ 03-custom_events/app/main.js
@@ -1,10 +1,16 @@
 const inputComponent = {
-  template: `<input class="input is-small" type="text" />`
+  template: `<input :placeholder="placeholder" class="input is-small" type="text" />`,
+  props: ['placeholder']
 }
 
 new Vue({
   el: '#app',
   components: {
     'input-component': inputComponent
+  },
+  data: {
+    notes: [],
+    timestamps: [],
+    placeholder: 'Enter a note'
   }
 })
