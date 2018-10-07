cfe36b1b9bf1c811e94cdb3a46e6818582e91bd2
Zhuo-Fei Hui <fei@posteo.de>
2018-10-07 21:40:50 +0200

    Emitting add-note event when hitting Enter

--- 03-custom_events/app/main.js
+++ 03-custom_events/app/main.js
@@ -1,6 +1,29 @@
 const inputComponent = {
-  template: `<input :placeholder="placeholder" class="input is-small" type="text" />`,
-  props: ['placeholder']
+  template:
+    `
+      <input
+        :placeholder="placeholder"
+        v-model="input"
+        @keyup.enter="monitorEnterKey"
+        class="input is-small"
+        type="text"
+      />
+    `,
+  props: ['placeholder'],
+  data () {
+    return {
+      input: ''
+    }
+  },
+  methods: {
+    monitorEnterKey() {
+      this.$emit('add-note', {
+        note: this.input,
+        timestamp: new Date().toLocaleString()
+      });
+      this.input = '';
+    }
+  }
 }
 
 new Vue({
