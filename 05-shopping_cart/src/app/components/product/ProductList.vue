84e5af558c1e99c36f186b6935dc5a271feec0d1
Zhuo-Fei Hui <fei@posteo.de>
2018-11-04 17:11:09 +0100

    Show actual productItems in productList

--- 05-shopping_cart/src/app/components/product/ProductList.vue
+++ 05-shopping_cart/src/app/components/product/ProductList.vue
@@ -4,22 +4,11 @@
       <i class="fa fa-2x fa-user-circle"></i>
     </div>
     <div class="product-list">
-      <div class="product-list--item">
-        <div>
-          <h2 class="has-text-weight-bold">The Fullstack Hoodie
-            <span class="tag
-                is-primary
-                is-pulled-right
-                has-text-white">
-              Add to Cart
-            </span>
-          </h2>
-          <p>Lightweight, breathable hoodie with the Fullstack Crest.
-          Guaranteed to keep you looking fresh while warm.</p>
-          <span class="has-text-primary has-text-weight-bold">
-            <i class="fa fa-usd"></i> 19.99
-          </span>
-        </div>
+      <div
+        v-for="productItem in productItems"
+        :key="productItem.id"
+        class="product-list--item">
+        <ProductListItem :productItem="productItem" />
       </div>
     </div>
     <div class="product-count has-text-right">
@@ -47,10 +36,6 @@ export default {
 </script>
 
 <style scoped>
-.tag {
-  cursor: pointer;
-}
-
 .products--header {
   border-bottom: 1px solid #E8E8E8;
   padding-bottom: 15px;
--- 05-shopping_cart/src/app/components/product/ProductListItem.vue
+++ 05-shopping_cart/src/app/components/product/ProductListItem.vue
@@ -1,8 +1,29 @@
 <template>
+  <div>
+    <h2 class="has-text-weight-bold">{{ productItem.title }}
+      <span class="tag
+          is-primary
+          is-pulled-right
+          has-text-white">
+        Add to Cart
+      </span>
+    </h2>
+    <p>{{ productItem.description }}</p>
+    <span class="has-text-primary has-text-weight-bold">
+      <i class="fa fa-usd"></i> 19.99
+    </span>
+  </div>
 </template>
 
 <script>
+export default {
+  name: 'ProductListItem',
+  props: ['productItem']
+}
 </script>
 
 <style scoped>
+.tag {
+  cursor: pointer;
+}
 </style>
