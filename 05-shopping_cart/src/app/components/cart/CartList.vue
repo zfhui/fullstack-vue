d483c242742b90174816b1468d618a932e159875
Zhuo-Fei Hui <fei@posteo.de>
2018-11-05 21:53:28 +0100

    Add CartListItems

--- 05-shopping_cart/src/app/components/cart/CartList.vue
+++ 05-shopping_cart/src/app/components/cart/CartList.vue
@@ -4,50 +4,36 @@
       <i class="fa fa-2x fa-shopping-cart"></i>
     </div>
     <ul>
-      <li class="cart-item">
-        <div>
-          <p class="cart-item--title is-inline">The Fullstack Hoodie</p>
-          <div class="is-pulled-right">
-            <i class="fa fa-arrow-circle-up cart-item--modify"></i>
-            <i class="fa fa-arrow-circle-down cart-item--modify"></i>
-          </div>
-          <div class="cart-item--content">
-            <span class="cart-item--price
-                has-text-primary
-                has-text-weight-bold">
-              19.99$ each
-            </span>
-            <span class="cart-item--quantity
-                has-text-grey
-                is-pulled-right">
-              Quantity: 2
-            </span>
-          </div>
-        </div>
+      <li v-for="cartItem in cartItems" :key="cartItem.id" class="cart-item">
+        <CartListItem :cartItem="cartItem" />
       </li>
       <div class="cart-details">
-        <p>Total Quantity: <span class="has-text-weight-bold">2</span></p>
+        <p>Total Quantity: <span class="has-text-weight-bold">{{ cartQuantity }}</span></p>
         <p class="cart-remove-all--text">
           <i class="fa fa-trash"></i>Remove all
         </p>
       </div>
     </ul>
     <button class="button is-primary">
-      Checkout (<span class="has-text-weight-bold">$</span>)
+      Checkout (<span class="has-text-weight-bold">${{ cartTotal }}</span>)
     </button>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
+import CartListItem from './CartListItem';
 
 export default {
   name: 'CartList',
   computed: {
-    ...mapGetters(['cartItems', 'cartTotal', 'cartQuanitity'])
+    ...mapGetters(['cartItems', 'cartTotal', 'cartQuantity'])
   },
   created () {
     this.$store.dispatch('getCartItems');
+  },
+  components: {
+    CartListItem
   }
 }
 </script>
---
+++ 05-shopping_cart/src/app/components/cart/CartListItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p class="cart-item--title is-inline">{{ cartItem.title }}</p>
+    <div class="is-pulled-right">
+      <i class="fa fa-arrow-circle-up cart-item--modify"></i>
+      <i class="fa fa-arrow-circle-down cart-item--modify"></i>
+    </div>
+    <div class="cart-item--content">
+      <span class="cart-item--price
+          has-text-primary
+          has-text-weight-bold">
+        {{ cartItem.price }}$ each
+      </span>
+      <span class="cart-item--quantity
+          has-text-grey
+          is-pulled-right">
+        Quantity: {{ cartItem.quantity }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CartListItem',
+  props: ['cartItem']
+}
+</script>
+
+<style scoped>
+.cart-item--modify {
+  cursor: pointer;
+}
+</style>
