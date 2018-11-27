274e41108218ca6ef9ae7a612049c099449fa8d8
Zhuo-Fei Hui <fei@posteo.de>
2018-11-28 00:22:03 +0100

    Add button-row and simple events on click

--- 06-form_handling/app/main.js
+++ 06-form_handling/app/main.js
@@ -1,3 +1,30 @@
+const ButtonRow = {
+  template: `
+    <div>
+      <button @click="onHoodieClick" class="ui button">Hoodie</button>
+      <button @click="onTeeClick" class="ui button">Tee</button>
+      <button @click="onFittedCapClick" class="ui button">FittedCap</button>
+      <button @click="onJacketClick" class="ui button">Jacket</button>
+    </div>`,
+    methods: {
+      onHoodieClick(evt) {
+        console.log('The user clicked button-hoodie', evt);
+      },
+      onTeeClick(evt) {
+        console.log('The user clicked button-tee', evt);
+      },
+      onFittedCapClick(evt) {
+        console.log('The user clicked button-fitted-cap', evt);
+      },
+      onJacketClick(evt) {
+        console.log('The user clicked button-hacket', evt);
+      }
+    }
+}
+
 new Vue({
   el: '#app',
+  components: {
+    'button-row': ButtonRow
+  }
 })
