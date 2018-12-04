2353690ec44343c218233d22a0be3b785a014425
Zhuo-Fei Hui <fei@posteo.de>
2018-12-04 23:51:19 +0100

    Trigger correct console event

--- 06-form_handling/02-input/main.js
+++ 06-form_handling/02-input/main.js
@@ -1,14 +1,20 @@
 const InputForm = {
   template: `
     <div class="input-form">
-      <form class="ui form">
+      <form @submit="submitForm" class="ui form">
         <div class="field">
           <input ref="newItem" type="text" placeholder="Add an item!">
         </div>
         <button class="ui button">Submit</button>
       </form>
     </div>
-  `
+  `,
+  methods: {
+    submitForm(evt) {
+      evt.preventDefault();
+      console.log(this.$refs.newItem.value)
+    }
+  }
 }
 
 new Vue({
